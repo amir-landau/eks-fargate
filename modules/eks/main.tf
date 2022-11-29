4b87f4eca486608aa41d1baac9be0b06085a8bc0
amirlandau <amirlandau5@gmail.com>
2022-11-29 13:18:26 +0200

    add kms module locally

--- modules/eks/main.tf
+++ modules/eks/main.tf
@@ -91,7 +91,7 @@ resource "aws_cloudwatch_log_group" "this" {
 ################################################################################
 
  module "kms" {
-   source  = "terraform-aws-modules/kms/aws"
+   source  = "./modules/kms"
    version = "1.0.2" # Note - be mindful of Terraform/provider version compatibility between modules
  
    create = local.create && var.create_kms_key
